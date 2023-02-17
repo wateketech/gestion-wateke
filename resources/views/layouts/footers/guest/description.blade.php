fa79173991731f3e65939c4690c625c3d2dced30
Alberto Licea <albertolicea00@gmail.com>
2023-02-16 21:32:30 -0500

    modifique secion del footer guest

--- resources/views/layouts/footers/guest/description.blade.php
+++ resources/views/layouts/footers/guest/description.blade.php
@@ -1,30 +1,21 @@
 <footer class="footer pt-8 position-sticky">
     <div class="container">
         <div class="row">
-            <div class="col-lg-8 mb-4 mx-auto text-center">
-                <a href="https://www.creative-tim.com" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Company
-                </a>
-                <a href="https://www.creative-tim.com/presentation" target="_blank"
-                    class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    About Us
-                </a>
-                <a href="https://www.creative-tim.com/presentation#pablo" target="_blank"
-                    class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Team
-                </a>
-                <a href="https://www.creative-tim.com/templates" target="_blank"
-                    class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Products
-                </a>
-                <a href="https://www.creative-tim.com/blog" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Blog
-                </a>
-                <a href="https://www.creative-tim.com/support-terms" target="_blank"
-                    class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                    Pricing
-                </a>
+            <div class="col-lg-6">
+                <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                        <a href="https://wateke.travel/quienes-somos/" class="nav-link text-muted"
+                            target="_blank">Quienes somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://wateke.travel/contactos/" class="nav-link text-muted"
+                            target="_blank">Donde estamos</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
 </footer>
+
+
+-
--- resources/views/layouts/footers/guest/with-socials.blade.php
+++ resources/views/layouts/footers/guest/with-socials.blade.php
@@ -2,57 +2,38 @@
       <div class="container">
           <div class="row">
               <div class="col-lg-8 mb-4 mx-auto text-center">
-                  <a href="https://www.creative-tim.com" target="_blank"
+                  <a href="https://wateke.travel/quienes-somos/" target="_blank"
                       class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                      Company
+                      Quienes somos
                   </a>
-                  <a href="https://www.creative-tim.com/presentation" target="_blank"
-                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                      About Us
-                  </a>
-                  <a href="https://www.creative-tim.com/presentation" target="_blank"
-                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                      Team
-                  </a>
-                  <a href="https://www.creative-tim.com/templates" target="_blank"
-                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                      Products
-                  </a>
-                  <a href="https://www.creative-tim.com/blog" target="_blank"
-                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                      Blog
-                  </a>
-                  <a href="https://www.creative-tim.com/support-terms" target="_blank"
-                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                      Pricing
-                  </a>
+                  <a href="https://wateke.travel/contactos/" target="_blank"
+                  class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                  Donde estamos
+              </a>
               </div>
               <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-                  <a href="https://dribbble.com/creativetim" target="_blank" class="text-secondary me-xl-4 me-4">
-                      <span class="text-lg fab fa-dribbble"></span>
+                  <a href="https://wateke.travel" target="_blank" class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fa fa-globe"></span>
                   </a>
-                  <a href="https://twitter.com/CreativeTim" target="_blank" class="text-secondary me-xl-4 me-4">
+                  <a href="https://twitter.com/WatekeTravel" target="_blank" class="text-secondary me-xl-4 me-4">
                       <span class="text-lg fab fa-twitter"></span>
                   </a>
-                  <a href="https://www.instagram.com/creativetimofficial/" target="_blank"
+                  <a href="https://www.linkedin.com/company/wateke-travel/" target="_blank"
                       class="text-secondary me-xl-4 me-4">
-                      <span class="text-lg fab fa-instagram"></span>
+                      <span class="text-lg fab fa-linkedin"></span>
                   </a>
-                  <a href="https://ro.pinterest.com/thecreativetim/" target="_blank"
-                      class="text-secondary me-xl-4 me-4">
-                      <span class="text-lg fab fa-pinterest"></span>
-                  </a>
-                  <a href="https://github.com/creativetimofficial" target="_blank" class="text-secondary me-xl-4 me-4">
+                  <a href="https://github.com/wateketech/" target="_blank" class="text-secondary me-xl-4 me-4">
                       <span class="text-lg fab fa-github"></span>
                   </a>
               </div>
           </div>
           <div class="row">
-              <div class="col-8 mx-auto text-center mt-1">
+            <div class="col-8 mx-auto text-center mt-1">
                   <p class="mb-0 text-secondary">
-                      Copyright © {{ now()->year }} Soft by <a style="color: #252f40;" href="https://www.creative-tim.com"
-                          class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp; <a style="color: #252f40;"
-                          href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>
+                      Copyright © {{ now()->year }} Made with ❤️ by
+                      <a href="https://github.com/albertolicea00" class="font-weight-bold ml-1" target="_blank">Alberto</a>
+                      &amp; <a href="https://github.com/Edes7191" class="font-weight-bold ml-1" target="_blank">Eduardo</a>
+                      <a style="color: #252f40;" href="https://wateke.travel/" class="text-muted ml-1" target="_blank">, Wateke Soluciones Tecnológicas</a>
               </div>
               </p>
           </div>
