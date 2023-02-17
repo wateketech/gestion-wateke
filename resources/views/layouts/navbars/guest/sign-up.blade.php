1440d19c63b9163b51ebd592bffc81355e49ef0b
Alberto Licea <albertolicea00@gmail.com>
2023-02-16 21:30:13 -0500

    modifique secion del refistro guest

--- resources/views/layouts/navbars/guest/sign-up.blade.php
+++ resources/views/layouts/navbars/guest/sign-up.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent mt-4">
     <div class="container">
-        <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white" href="{{ route('dashboard') }}">
-            Soft UI Dashboard
+        <a class="navbar-brand d-flex flex-column font-weight-bolder ms-lg-0 ms-3 text-white" href="{{ route('dashboard') }}">
+            Gestion :: Wateke Travel
         </a>
         <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
             data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
@@ -19,34 +19,35 @@
                         <a class="nav-link text-white d-flex align-items-center me-2 active" aria-current="page"
                             href="{{ route('dashboard') }}">
                             <i class="fa fa-chart-pie opacity-6  me-1"></i>
-                            Dashboard
+                            {{ __('Dashboard')}}
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white me-2" href="{{ route('profile') }}">
                             <i class="fa fa-user opacity-6  me-1"></i>
-                            Profile
+                            {{ __('Profile')}}
                         </a>
                     </li>
                 @endif
                 <li class="nav-item">
                     <a class="nav-link text-white me-2" href="{{ auth()->user() ? route('static-sign-up') : route('sign-up') }}">
                         <i class="fas fa-user-circle opacity-6  me-1"></i>
-                        Sign Up
+                        {{ __('Sign up')}}
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white me-2" href="{{ auth()->user() ? route('sign-in') : route('login') }}">
                         <i class="fas fa-key opacity-6  me-1"></i>
-                        Sign In
+                        {{ __('Sign in')}}
                     </a>
                 </li>
             </ul>
             <ul class="navbar-nav d-lg-block d-none">
                 <li class="nav-item">
-                    <a href="https://www.creative-tim.com/product/soft-ui-dashboard-laravel"
-                        class="btn btn-sm btn-round mb-0 me-1 bg-gradient-light" target="_blank">Free download</a>
-                </li>
+                    <small class="navbar-brand ms-lg-0 ms-3 disabled text-decoration-line-through text-white"> Descargar: </small>
+                    <a href="#"
+                        class="btn btn-sm btn-round mb-0 me-1 bg-gradient-light disabled" target="_blank"> APP MOVIL
+                    </a>
             </ul>
         </div>
     </div>
