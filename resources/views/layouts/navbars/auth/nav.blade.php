f3cd84e030b69bc35d43a0a74a8a49a979440bbd
Alberto Licea <albertolicea00@gmail.com>
2023-02-16 21:14:42 -0500

    modifique secion del nav auth

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid py-1 px-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-md"><a class="opacity-5 text-dark" href="javascript:;">Pages</a>
+                    <li class="breadcrumb-item text-md"><a class="opacity-5 text-dark" href="javascript:;">{{__("Pages")}}</a>
                     </li>
                     <li class="breadcrumb-item text-sm text-dark active text-capitalize" aria-current="page">
                         {{ str_replace('-', ' ', Route::currentRouteName()) }}</li>
@@ -13,16 +13,10 @@
                     {{ str_replace('-', ' ', Route::currentRouteName()) }}</h6>
             </nav>
             <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end" id="navbar">
-                <div class="nav-item d-flex align-self-end">
-                    <a href="https://www.creative-tim.com/product/soft-ui-dashboard-laravel" target="_blank"
-                        class="btn btn-primary active mb-0 text-white" role="button" aria-pressed="true">
-                        Download
-                    </a>
-                </div>
                 <div class="ms-md-3 pe-md-3 d-flex align-items-center">
                     <div class="input-group">
                         <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-                        <input type="text" class="form-control" placeholder="Type here...">
+                        <input type="text" class="form-control" placeholder="{{ __('Type here...')}}">
                     </div>
                 </div>
                 <ul class="navbar-nav justify-content-end">
@@ -49,13 +43,16 @@
                         <a href="javascript:;" class="nav-link text-body p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fa fa-bell cursor-pointer"></i>
                         </a>
-                        <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
-                            <li class="mb-2">
+
+<div hidden> informacion y notificaciones de la persona autenticada
+                    <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
+                        <li class="mb-2">
                                 <a class="dropdown-item border-radius-md" href="javascript:;">
                                     <div class="d-flex py-1">
                                         <div class="my-auto">
                                             <img src="../assets/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
                                         </div>
+                                        
                                         <div class="d-flex flex-column justify-content-center">
                                             <h6 class="text-sm font-weight-normal mb-1">
                                                 <span class="font-weight-bold">New message</span> from Laur
@@ -129,6 +126,7 @@
                                 </a>
                             </li>
                         </ul>
+</div>
                     </li>
                 </ul>
             </div>
