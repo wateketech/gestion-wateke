729dfd68aea687a9fdbcf2ceeb142c1c66bf80df
Alberto Licea <albertolicea00@gmail.com>
2023-02-16 19:11:26 -0500

    modifique secion del sign-up

--- resources/views/livewire/auth/sign-up.blade.php
+++ resources/views/livewire/auth/sign-up.blade.php
@@ -1,14 +1,13 @@
-  <section class="h-100-vh mb-8">
+<section class="h-100-vh mb-8">
       <div class="page-header align-items-start section-height-50 pt-5 pb-11 m-3 border-radius-lg"
-          style="background-image: url('../assets/img/curved-images/curved14.jpg');">
+          style="background-image: url('../assets/img/wallpapers/wallpaper-2.jpg');">
           <span class="mask bg-gradient-dark opacity-6"></span>
           <div class="container">
               <div class="row justify-content-center">
                   <div class="col-lg-5 text-center mx-auto">
                       <h1 class="text-white mb-2 mt-5">{{ __('Welcome!') }}</h1>
                       <p class="text-lead text-white">
-                          {{ __('Use these awesome forms to login or create new account in your
-                          project for free.') }}
+                          {{ __('Use this amazing forms to log in or request an account and manage reservations with') }} Wateke Travel.
                       </p>
                   </div>
               </div>
@@ -18,9 +17,11 @@
           <div class="row mt-lg-n10 mt-md-n11 mt-n10">
               <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
                   <div class="card z-index-0">
-                      <div class="card-header text-center pt-4">
-                          <h5>{{ __('Register with') }}</h5>
-                      </div>
+
+                    <div class="card-header text-center pt-4">
+                        <h5>{{ __('Register with') }} Wateke Travel</h5>
+                    </div>
+{{--
                       <div class="row px-xl-5 px-sm-4 px-3">
                           <div class="col-3 ms-auto px-1">
                               <a class="btn btn-outline-light w-100" href="javascript:;">
@@ -81,31 +82,35 @@
                           <div class="mt-2 position-relative text-center">
                               <p
                                   class="text-sm font-weight-bold mb-2 text-secondary text-border d-inline z-index-2 bg-white px-3">
-                                  or
+                                  {{ __('or')}}
                               </p>
                           </div>
                       </div>
+--}}
                       <div class="card-body">
 
                           <form wire:submit.prevent="register" action="#" method="POST" role="form text-left">
                               <div class="mb-3">
+                                    <sub>{{ __('Name') }}:</sub>
                                   <div class="@error('name') border border-danger rounded-3  @enderror">
-                                      <input wire:model="name" type="text" class="form-control" placeholder="Name"
+                                      <input wire:model="name" type="text" class="form-control" placeholder={{ __('Name') }}
                                           aria-label="Name" aria-describedby="email-addon">
                                   </div>
                                   @error('name') <div class="text-danger">{{ $message }}</div> @enderror
                               </div>
                               <div class="mb-3">
+                                    <sub>{{ __('Email') }}:</sub>
                                   <div class="@error('email') border border-danger rounded-3 @enderror">
-                                      <input wire:model="email" type="email" class="form-control" placeholder="Email"
+                                      <input wire:model="email" type="email" class="form-control" placeholder={{ __('Email') }}
                                           aria-label="Email" aria-describedby="email-addon">
                                   </div>
                                   @error('email') <div class="text-danger">{{ $message }}</div> @enderror
                               </div>
                               <div class="mb-3">
+                                    <sub>{{ __('Password') }}:</sub>
                                   <div class="@error('password') border border-danger rounded-3 @enderror">
                                       <input wire:model="password" type="password" class="form-control"
-                                          placeholder="Password" aria-label="Password"
+                                          placeholder={{ __('Password') }} aria-label="Password"
                                           aria-describedby="password-addon">
                                   </div>
                                   @error('password') <div class="text-danger">{{ $message }}</div> @enderror
@@ -115,13 +120,11 @@
                                       checked>
                                   <label class="form-check-label" for="flexCheckDefault">
                                       {{ __('I agree the') }} <a href="javascript:;"
-                                          class="text-dark font-weight-bolder">{{ __('Terms
-                                          and
-                                          Conditions') }}</a>
+                                          class="text-dark font-weight-bolder">{{ __('Terms and Conditions') }}</a>
                                   </label>
                               </div>
                               <div class="text-center">
-                                  <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                                  <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">{{ __('Sign up') }}</button>
                               </div>
                               <p class="text-sm mt-3 mb-0">{{ __('Already have an account? ') }}<a
                                       href="{{ route('login') }}"
