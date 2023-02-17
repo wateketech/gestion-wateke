1a2430e56d117f1362f6a71fb4fd350bf78fb434
Alberto Licea <albertolicea00@gmail.com>
2023-02-17 16:39:03 -0500

    actualizacion en bbdd

--- database/migrations/2023_02_16_221651_create_grupo_gestion_table.php
+++ database/migrations/2023_02_16_221651_create_grupo_gestion_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('grupo_gestion', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('entidad_id');
             $table->string('nombre');
 
             $table->timestamps();
