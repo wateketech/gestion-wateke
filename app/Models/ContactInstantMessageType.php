6558568a2635a2088821d83e069fe501d7eebe5c
Alberto Licea <albertolicea00@gmail.com>
2023-05-19 15:28:37 -0400

    modelo iniciado

---
+++ app/Models/ContactInstantMessageType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactInstantMessageType extends Model
+{
+    use HasFactory;
+}
--- database/seeders/Entity_rrss_TypesSeeder.php
+++ database/seeders/Entity_rrss_TypesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\EntityRrssType as EntityRrssTypes;
 
 class Entity_rrss_TypesSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class Entity_rrss_TypesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        EntityRrssTypes::createMany([]);
     }
 }
