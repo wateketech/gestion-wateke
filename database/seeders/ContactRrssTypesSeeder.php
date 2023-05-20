8bceb77667a5b633d1b01f8cee81b68eb2e2c1c3
Alberto Licea <albertolicea00@gmail.com>
2023-05-20 09:56:22 -0400

    rrss seeders

--- database/seeders/ContactRrssTypesSeeder.php
+++ database/seeders/ContactRrssTypesSeeder.php
@@ -15,6 +15,44 @@ class ContactRrssTypesSeeder extends Seeder
      */
     public function run()
     {
-        ContactRrssTypes::createMany([]);
+        ContactRrssTypes::createMany([
+            [
+                'label' => 'LinkedIn',
+                'icon' => '<i class="fab fa-linkedin-in" style="transform:scale(1.5)"></i>',
+                'color' => '#0077b5',
+            ],[
+                'label' => 'Twitter',
+                'icon' => '<i class="fab fa-twitter" style="transform:scale(1.5)"></i>',
+                'color' => '#1da1f2',
+            ],[
+                'label' => 'Facebook',
+                'icon' => '<i class="fab fa-facebook-f" style="transform:scale(1.5)"></i>',
+                'color' => '#3b5998',
+            ],[
+                'label' => 'Instagram',
+                'icon' => '<i class="fab fa-instagram" style="transform:scale(1.8)"></i>',
+                'color' => '#e4405f',
+            ],[
+                'label' => 'Youtube',
+                'icon' => '<i class="fab fa-youtube" style="transform:scale(1.5)"></i>',
+                'color' => '#c4302b',
+            ],[
+                'label' => 'Tiktok',
+                'icon' => '<i class="fab fa-tiktok" style="transform:scale(1.5)"></i>',
+                'color' => '#000000',
+            ],[
+                'label' => 'Snapchat',
+                'icon' => '<i class="fab fa-snapchat-ghost" style="transform:scale(1.5)"></i>',
+                'color' => '#dbd82c',
+            ],[
+                'label' => 'Pinterest',
+                'icon' => '<i class="fab fa-pinterest" style="transform:scale(1.5)"></i>',
+                'color' => '#bd081c',
+            ],[
+                'label' => 'Google Plus',
+                'icon' => '<i class="fab fa-google-plus-g" style="transform:scale(1.5)"></i>',
+                'color' => '#dd4b39',
+            ]
+        ]);
     }
 }
--- database/seeders/EntityRrssTypesSeeder.php
+++ database/seeders/EntityRrssTypesSeeder.php
@@ -15,6 +15,36 @@ class EntityRrssTypesSeeder extends Seeder
      */
     public function run()
     {
-        EntityRrssTypes::createMany([]);
+        EntityRrssTypes::createMany([
+            [
+                'label' => 'LinkedIn',
+                'icon' => '<i class="fab fa-linkedin-in" style="transform:scale(1.5)"></i>',
+                'color' => '#0077b5',
+            ],[
+                'label' => 'Twitter',
+                'icon' => '<i class="fab fa-twitter" style="transform:scale(1.5)"></i>',
+                'color' => '#1da1f2',
+            ],[
+                'label' => 'Facebook',
+                'icon' => '<i class="fab fa-facebook-f" style="transform:scale(1.5)"></i>',
+                'color' => '#3b5998',
+            ],[
+                'label' => 'Instagram',
+                'icon' => '<i class="fab fa-instagram" style="transform:scale(1.8)"></i>',
+                'color' => '#e4405f',
+            ],[
+                'label' => 'Google Plus',
+                'icon' => '<i class="fab fa-google-plus-g" style="transform:scale(1.5)"></i>',
+                'color' => '#dd4b39',
+            ],[
+                'label' => 'Youtube',
+                'icon' => '<i class="fab fa-youtube" style="transform:scale(1.5)"></i>',
+                'color' => '#c4302b',
+            ],[
+                'label' => 'Pinterest',
+                'icon' => '<i class="fab fa-pinterest" style="transform:scale(1.5)"></i>',
+                'color' => '#bd081c',
+            ]
+        ]);
     }
 }
